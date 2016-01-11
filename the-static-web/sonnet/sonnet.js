e4fca4def6285408a3a66732fdc59baa5bcf4794
Palmer Harrell <palmerharrell@me.com>
2016-01-11 13:20:21 -0600

    Initial set up - Sonnet JS exercise

---
+++ the-static-web/sonnet/sonnet.js
@@ -0,0 +1,12 @@
+
+// Take the contents of the sonnet div and place it in a variable
+
+// Find and output the starting position of the word "orphans"
+
+// Output the number of characters in the sonnet
+
+// Replace the first occurance of the string "winter" with "yuletide"
+
+// Replace all occurances of the string "the" with "a large"
+
+// Set the content of the sonnet div with the new string
