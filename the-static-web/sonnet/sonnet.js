7a8c849bfa0ae2010075898df5dca254ee3b6b7a
Palmer Harrell <palmerharrell@me.com>
2016-01-11 13:40:04 -0600

    Changed Regex in Sonnet JS exercise

--- the-static-web/sonnet/sonnet.js
+++ the-static-web/sonnet/sonnet.js
@@ -12,7 +12,7 @@ console.log('length of sonnet:', sonnetText.length);
 sonnetText = sonnetText.replace('winter', 'yuletide');
 
 // Replace all occurances of the string "the" with "a large"
-sonnetText = sonnetText.replace(/the/g, 'a large');
+sonnetText = sonnetText.replace(/the /g, 'a large ');
 
 // Set the content of the sonnet div with the new string
 document.getElementById("sonnet").innerHTML = sonnetText;
