41703473a6be482b3166611fbe338ab411f8a314
Palmer Harrell <palmerharrell@me.com>
2016-01-14 13:07:15 -0600

    Finished Boy Bands and Vegetables JS exercise

--- the-static-web/boybands/boybands.js
+++ the-static-web/boybands/boybands.js
@@ -11,19 +11,20 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 
   // Add the band names into the correct <div>
-  currentBand = ???;
-
+  currentBand = bands[loopTracker];
+  bandElement.innerHTML += "<p>" + currentBand + "</p>";
 
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+  currentVeggie = vegetables[loopTracker];
+  veggieElement.innerHTML += "<p>" + currentVeggie + "</p>"; 
 
 }
