b87a8c0ce5c114fa702a6164704546c4a9bf2467
Palmer Harrell <palmerharrell@me.com>
2016-01-16 13:00:07 -0600

    Finished Counting coins exercise

--- the-static-web/coins/coins.js
+++ the-static-web/coins/coins.js
@@ -16,14 +16,74 @@
 
 */
 
-function coinCounter () {
+var money = 0.49;
+console.log("$" + money);
+
+function coinCounter (money) {
   // Initialize a JavaScript object to hold the coins
   var coinPurse = {};
-
   coinPurse.quarters = 0;
+  coinPurse.dimes = 0;
+  coinPurse.nickels = 0;
+  coinPurse.pennies = 0;
+
+  money *= 100;
+  // debugger;
+
+  do {
+    if (money >= 25) {
+      coinPurse.quarters++;
+      money -= 25;
+      // debugger;
+    } else if (money < 25 && money >= 10) {
+      coinPurse.dimes++;
+      money -= 10;
+      // debugger;
+    } else if (money < 10 && money >= 5) {
+      coinPurse.nickels++;
+      money -= 5;
+      // debugger;
+    } else if (money < 5) {
+      coinPurse.pennies++;
+      money -= 1;
+      // debugger;
+    }
+  } while (money > 0);
 
   return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+var coins = coinCounter(money);
+
+console.log("Quarters: " + coins.quarters);
+console.log("Dimes: " + coins.dimes);
+console.log("Nickels: " + coins.nickels);
+console.log("Pennies: " + coins.pennies);
+
+
+
+
+// switch (true) {
+//       case money >= 0.25:
+//         coinPurse.quarters++;
+//         money = money - 0.25;
+//         debugger;
+
+//       case money >= 0.10:
+//         coinPurse.dimes++;
+//         money = money - 0.10;
+//         debugger;
+      
+//       case money >= 0.05:
+//         coinPurse.nickels++;
+//         money = money - 0.05;
+//         debugger;
+
+//       case money >= 0.01:
+//         coinPurse.nickels++;
+//         money = money - 0.01;
+//         debugger;
+
+//       default:
+//         break;
+//     }
