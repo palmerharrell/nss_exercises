f0ab9e6ae9268f3a6e90f1d9a01595777f14b6c9
Palmer Harrell <palmerharrell@me.com>
2016-01-17 15:39:34 -0600

    Looped over scores array and counted grades

--- the-static-web/grades/grades.js
+++ the-static-web/grades/grades.js
@@ -9,13 +9,47 @@
 // Start with array of random scores in your JavaScript
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
 
+// Number of each:
+var a = 0;
+var b = 0;
+var c = 0;
+var d = 0;
+var f = 0;
+
+for (var i = 0; i < scores.length; i++) {
+	switch(true){
+		case scores[i] > 90:
+			a++;
+			break;
+		case scores[i] > 80:
+			b++;
+			break;
+		case scores[i] > 70:
+			c++;
+			break;
+		case scores[i] > 60:
+			d++;
+			break;
+		default:
+			f++;
+			break;
+	}
+}
+
 // Use console.log to output the following criteria:
 
-// How many of each grade?
+console.log("List of scores: ", scores);
 
+// How many of each grade?
+console.log('There are ' + a + ' "A"s.');
+console.log('There are ' + b + ' "B"s.');
+console.log('There are ' + c + ' "C"s.');
+console.log('There are ' + d + ' "D"s.');
+console.log('There are ' + f + ' "F"s.');
 
 // What is the lowest grade?
 
 
 // What is the highest grade?
+
 
