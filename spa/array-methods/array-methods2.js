3c2164d6ee8df249e0ed95d317c64d86d87ee4db
Palmer Harrell <palmerharrell@me.com>
2016-01-26 11:21:57 -0600

    Re-wrote js code for practice

---
+++ spa/array-methods/array-methods2.js
@@ -0,0 +1,32 @@
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+
+/* 
+ Use the forEach method to add the name of each planet
+ to a div element in your HTML with an id of "planets".
+*/
+var el = document.getElementById("planets");
+
+planets.forEach(function(currentItem) {
+	el.innerHTML += `${currentItem} `;
+})
+
+// Use the map method to create a new array where the first letter of each planet is capitalized
+var capitalizedPlanets = planets.map(function(currentItem) {
+	return currentItem.charAt(0).toUpperCase() + currentItem.slice(1);
+})
+console.log(capitalizedPlanets);
+
+// Use the filter method to create a new array that contains planets with the letter 'e'
+var ePlanets = planets.filter(function(currentItem) {
+	return currentItem.indexOf("e") !== -1;
+})
+console.log(ePlanets);
+
+// Use the reduce method to create a sentence from the words in the following array
+
+var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+
+var sentence = words.reduce(function(currentItem, previousItem) {
+	return `${currentItem} ${previousItem}`;
+})
+console.log(sentence);
